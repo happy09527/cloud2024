c40c76ec49c0a1d8ce887d528ea7af3514ff432f
Zhangx <1756607826@qq.com>
2025-03-30 15:00:02 +0800

    nacos 配置

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
@@ -8,8 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 @RestController
-public class OrderNacosController
-{
+public class OrderNacosController {
     @Resource
     private RestTemplate restTemplate;
 
@@ -17,9 +16,8 @@ public class OrderNacosController
     private String serverURL;
 
     @GetMapping("/consumer/pay/nacos/{id}")
-    public String paymentInfo(@PathVariable("id") Integer id)
-    {
+    public String paymentInfo(@PathVariable("id") Integer id) {
         String result = restTemplate.getForObject(serverURL + "/pay/nacos/" + id, String.class);
-        return result+"\t"+"    我是OrderNacosController83调用者。。。。。。";
+        return result + "\t" + "    我是OrderNacosController83调用者。。。。。。";
     }
 }
