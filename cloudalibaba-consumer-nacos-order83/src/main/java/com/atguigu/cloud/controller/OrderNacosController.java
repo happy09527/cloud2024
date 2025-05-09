1e3c615b8c895ce33ff9347e7791f90c44ce8fa5
Zhangx <1756607826@qq.com>
2025-05-09 23:52:29 +0800

    sentinel+feign 服务降级

--- cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
+++ cloudalibaba-consumer-nacos-order83/src/main/java/com/atguigu/cloud/controller/OrderNacosController.java
@@ -1,5 +1,7 @@
 package com.atguigu.cloud.controller;
 
+import com.atguigu.cloud.api.PayFeignSentinelApi;
+import com.atguigu.cloud.resp.ResultData;
 import jakarta.annotation.Resource;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,6 +13,8 @@ import org.springframework.web.client.RestTemplate;
 public class OrderNacosController {
     @Resource
     private RestTemplate restTemplate;
+    @Resource
+    private PayFeignSentinelApi payFeignSentinelApi;
 
     @Value("${service-url.nacos-user-service}")
     private String serverURL;
@@ -19,5 +23,11 @@ public class OrderNacosController {
     public String paymentInfo(@PathVariable("id") Integer id) {
         String result = restTemplate.getForObject(serverURL + "/pay/nacos/" + id, String.class);
         return result + "\t" + "    我是OrderNacosController83调用者。。。。。。";
+    }
+
+    @GetMapping(value = "/consumer/pay/nacos/get/{orderNo}")
+    public ResultData getPayByOrderNo(@PathVariable("orderNo") String orderNo)
+    {
+        return payFeignSentinelApi.getPayByOrderNo(orderNo);
     }
 }
